a567c361c4e263a8530e4140f7cdcf0997277b67
psii <psii@posteo.de>
2021-10-18 23:14:33 +0200

    Cleanup

--- src/main.lisp
+++ src/main.lisp
@@ -5,50 +5,54 @@
 (defparameter *universe-step* 0.014)
 (defparameter *step-split* 10)
 
-(defvar *player-pos* (gamekit:vec2 200.0 200.0))
+(defvar *player* nil)
+(defvar *universe* nil)
 
 (gamekit:defgame jupiter-game ()
-  ())
+  ()
+  (:viewport-title "Jupiter Candle"))
 
 (defmethod gamekit:draw ((this jupiter-game))
-  (gamekit:draw-text "o_O" *player-pos*))
+  (render *player*))
 
 (defmethod gamekit:post-initialize ((this jupiter-game))
-  ;;(setq *universe* (ge.phy:make-universe :2d))
-  ;;(setq *player* (make-instance 'player))
-  
-  (gamekit:bind-button :up :pressed
-                       (lambda ()
-                         (setf (gamekit:y *player-pos*) (+ (gamekit:y *player-pos*) 10))))
+  (setq *universe* (ge.phy:make-universe :2d))
+  (setq *player* (make-player :position (gamekit:vec2 100.0 100.0) :universe *universe*))
 
-  (gamekit:bind-button :down :pressed
-                       (lambda ()
-                         (setf (gamekit:y *player-pos*) (- (gamekit:y *player-pos*) 10))))
-
-  (gamekit:bind-button :left :pressed
-                       (lambda ()
-                         (setf (gamekit:x *player-pos*) (- (gamekit:x *player-pos*) 10))))
-
-  (gamekit:bind-button :right :pressed
-                       (lambda ()
-                         (setf (gamekit:x *player-pos*) (+ (gamekit:x *player-pos*) 10)))))
+  (flet ((bind-button-to-move (btn xoff yoff)
+           (gamekit:bind-button btn :pressed
+                                (lambda ()
+                                  (move-player *player* (gamekit:vec2 xoff yoff))))))
+    (bind-button-to-move :up 0 10)
+    (bind-button-to-move :down 0 -10)
+    (bind-button-to-move :left -10 0)
+    (bind-button-to-move :right 10 0)))
 
 
 (defclass player ()
-  (body
-   shape))
+  ((body :initarg :body :reader body)
+   (shape :initarg :shape)))
 
-;; (defmethod initialize-instance :after ((this player)) &key universe position
-;;   (with-slots (body shape) this
-;;     (setf body (ge.phy:make-rigid-body universe :mass 10.0)
-;;           shape (ge.phy:make-box-shape universe 20 20 :body body)
-;;           (ge.phy:body-position body) position)))
+(defun make-player (&key position (universe *universe*))
+  (let* ((body  (ge.phy:make-rigid-body universe))
+         (shape (ge.phy:make-box-shape universe 20 20 :body body)))
+    (setf (ge.phy:body-position body) position)
+    (make-instance 'player :body body :shape shape)))
 
-;; (defmethod render ((this player))
-;;   (with-slots (body) this
-;;     (let ((position (ge.phy:body-position body)))
-;;       (gamekit:draw-rect position 20 20)
-;;       (gamekit:draw-text "o_O" position))))
+(defun player-position (player)
+  (ge.phy:body-position (body player)))
+
+(defun set-player-position (player position)
+  (setf (ge.phy:body-position (body player)) position))
+
+(defun move-player (player offset)
+  (let ((position (player-position player)))
+    (set-player-position player (gamekit:add position offset))))
+
+(defmethod render ((this player))
+  (let ((position (player-position this)))
+    (gamekit:draw-rect position 20 20)
+    (gamekit:draw-text "o_O" position)))
 
 (defun main ()
   (gamekit:start 'jupiter-game))
