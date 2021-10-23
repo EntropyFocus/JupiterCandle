246649ffa41b044f0219d356312901146ad432e2
psii <psii@posteo.de>
2021-10-23 17:56:50 +0200

    Update player.lisp

--- src/player.lisp
+++ src/player.lisp
@@ -46,9 +46,10 @@
 
 (defun player-apply-impulse (player offset &key reset-vx reset-vy)
   (with-slots (body) player
-    (let ((vx (if reset-vx 0 (gamekit:x (player-speed player))))
-          (vy (if reset-vy 0 (gamekit:y (player-speed player)))))
-      (setf (ge.phy:body-linear-velocity body) (gamekit:vec2 vx vy)))
+    (when (or reset-vx reset-vy)
+      (let ((vx (if reset-vx 0 (gamekit:x (player-speed player))))
+            (vy (if reset-vy 0 (gamekit:y (player-speed player)))))
+        (setf (ge.phy:body-linear-velocity body) (gamekit:vec2 vx vy))))
     (ge.phy:apply-force (body player) (gamekit:mult offset 10000))))
 
 (defmethod render ((this player))
