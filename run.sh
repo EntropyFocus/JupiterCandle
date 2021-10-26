4cfb056b8f1747b065e9750f07f587e67f18ef3e
Brad Svercl <bradsvercl@gmail.com>
2021-10-25 21:00:41 -0500

    Closing quote for string was in the wrong spot

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sbcl --load "jupiter-candle.asd" --eval "(ql:quickload :jupiter-candle)" --eval "(jupiter-candle:main")
+sbcl --load "jupiter-candle.asd" --eval "(ql:quickload :jupiter-candle)" --eval "(jupiter-candle:main)"
